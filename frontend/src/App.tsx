9950f6a3ba16bf1d75c1d9707144d6b3ac7ffe9d
DK-Guilherme <88940931+DK-Guilherme@users.noreply.github.com>
2021-11-05 15:25:27 -0300

    Update title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <>
     <NavBar />
     <div className="container">
-      <h1 className="text-primary py-3">DashBoard De Vendas</h1>
+      <h1 className="text-primary py-3">Dashboard De Vendas</h1>
 
       <div className="row px-3">
         <div className="col-sm-6">
