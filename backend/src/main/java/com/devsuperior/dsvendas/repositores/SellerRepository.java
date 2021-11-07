c38501ddf417cd413d6d41b7df30f202d0368613
DK-Guilherme <88940931+DK-Guilherme@users.noreply.github.com>
2021-11-07 18:33:47 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repositores/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsvendas.repositores;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerServices.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositores.SellerRepository;
+
+@Service
+public class SellerServices {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
