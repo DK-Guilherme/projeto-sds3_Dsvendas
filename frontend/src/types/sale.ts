1bc783a0ca8722f0b43194b8ec431113dc88b42f
DK-Guilherme <88940931+DK-Guilherme@users.noreply.github.com>
2021-11-08 21:03:36 -0300

    Barchart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,11 @@
 export type SalesSum = {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
 }
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
+}
+
